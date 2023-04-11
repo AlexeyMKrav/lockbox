a4348c086cd881bc39b8697e924b0970fb45b2d5
Alexey Kravchenko <alexey.mkrav@gmail.com>
2023-04-11 17:07:51 +0300

    New project

---
+++ api/begin.ps1
@@ -0,0 +1,5 @@
+﻿python -m venv venv
+./venv/Scripts/Activate.ps1
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r requirements.txt
+Get-Command python
+#pip freeze > requirements.txt
