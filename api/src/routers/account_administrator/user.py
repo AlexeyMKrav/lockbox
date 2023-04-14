2818e0aab1a79397af4b7007e4a912124a96dc44
Alexey Kravchenko <alexey.mkrav@gmail.com>
2023-04-14 16:12:09 +0300

    refactoring db_user

--- api/src/routers/account_administrator/user.py
+++ api/src/routers/account_administrator/user.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.orm.session import Session
 from src.db.database import get_db
+from src.db.repositories.account import db_user
 from src.routers.account_administrator.schemas import UserBase
 
 router = APIRouter(
@@ -13,24 +14,24 @@ router = APIRouter(
 
 @router.post('/')
 def create(request: UserBase, db: Session = Depends(get_db)):
-    return db_account.create(db, request)
+    return db_user.create(db, request)
 
 
 @router.get('/{id}')
 def get(id: UUID, db: Session = Depends(get_db)):
-    return db_account.get(db, id)
+    return db_user.get(db, id)
 
 
 @router.get('/all')
 def get_all(db: Session = Depends(get_db)):
-    return db_account.get_all(db)
+    return db_user.get_all(db)
 
 
 @router.put('/{id}')
 def update(id: UUID, request: 'UserBase', db: Session = Depends(get_db)):
-    return db_account.update(db, id, request)
+    return db_user.update(db, id, request)
 
 
 @router.delete('/{id}')
 def delete(id: UUID, db: Session = Depends(get_db)):
-    db_account.delete(db, id)
+    db_user.delete(db, id)
